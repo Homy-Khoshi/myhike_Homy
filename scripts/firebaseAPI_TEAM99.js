0dadabb5d9459de2dc7169dddd6ae3a109ab1229
Homayoun Khoshi <Khoshihomayoun@gmail.com>
2024-03-18 10:39:49 -0700

    demo0

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyB26DHz5f7iJa-HZN-z-OlJaCt_JRB_7L4",
+  authDomain: "homy-hikes.firebaseapp.com",
+  projectId: "homy-hikes",
+  storageBucket: "homy-hikes.appspot.com",
+  messagingSenderId: "325952376242",
+  appId: "1:325952376242:web:efe26283160ca15f7b68ab"
 };
 
 //--------------------------------------------
